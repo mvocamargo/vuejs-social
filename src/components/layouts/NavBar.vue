8a591169d50ce9029d775941db8c12d26d82f8b4
Marcus Camargo <marcus@agenciavisia.com.br>
2018-06-28 19:12:29 -0300

    Componente - NavBar

---
+++ src/components/layouts/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <nav :class="cor || 'orange'">
+            <div class="nav-wrapper container">
+            <a :href="url || '#'" class="brand-logo">{{ logo || 'Site'}}</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="sass.html">Sass</a></li>
+                <li><a href="badges.html">Components</a></li>
+                <li><a href="collapsible.html">JavaScript</a></li>
+            </ul>
+            </div>
+        </nav>
+    </div>
+</template>
+<script>
+    export default{
+        name: 'NavBar',
+        props: ['logo', 'url', 'cor'],
+        data (){
+            return{
+
+            }
+        }
+    }        
+</script>
