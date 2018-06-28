8db0a7950a3a2a7476f55fd44a7cdc2eb6f8c808
Marcus Camargo <marcus@agenciavisia.com.br>
2018-06-28 19:30:16 -0300

    Atualização - App.vue
    Inclusão de topo e rodapé

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,29 @@
 <template>
-  <div>
-    <router-view/>
-  </div>
+    <div>
+        <header>
+            <nav-bar cor="pink" url="https://www.google.com" logo="Social"></nav-bar>
+        </header>
+        <main>
+            <router-view/>
+        </main>
+        <footer-vue cor="purple" logo="Social" descricao="Descrição do rolê">
+            <li><a class="grey-text text-lighten-3" href="#!">Link</a></li>
+            <li><a class="grey-text text-lighten-3" href="#!">Link</a></li>
+            <li><a class="grey-text text-lighten-3" href="#!">Link</a></li>
+            <li><a class="grey-text text-lighten-3" href="#!">Link</a></li>
+        </footer-vue>
+    </div>
 </template>
 
 <script>
+import NavBar from '@/components/layouts/NavBar'
+import FooterVue from '@/components/layouts/FooterVue'
 export default {
-  name: 'App'
+	name: 'App',
+	components:{
+		NavBar,
+		FooterVue
+	}
 }
 </script>
 
