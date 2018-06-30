912a1b8131dabc4746b87f4fc939c7f1f6d9da94
Marcus Camargo <mvocamargo@gmail.com>
2018-06-30 15:48:06 -0300

    Alteração - Home - Inclusão do Componente de publicação de conteúdo

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,16 +1,21 @@
 <template>
-	<card-conteudo-vue perfil-foto="https://materializecss.com/images/yuna.jpg" perfil-nome="Usuário dos Santos" post-data="12/01/2018 00:50">
-		<card-detalhe-vue 
-			post-imagem="https://materializecss.com/images/sample-1.jpg"
-			post-titulo="É isso aí"
-			post-texto="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae fugit necessitatibus optio nostrum eius porro, deleniti odit, perferendis ipsa minima corporis magni commodi, numquam iure animi itaque molestias saepe ex."
-		/>
-	</card-conteudo-vue>
+	<div>
+		<publicar-conteudo-vue></publicar-conteudo-vue>
+		<card-conteudo-vue perfil-foto="https://materializecss.com/images/yuna.jpg" perfil-nome="Usuário dos Santos" post-data="12/01/2018 00:50">
+			<card-detalhe-vue 
+				post-imagem="https://materializecss.com/images/sample-1.jpg"
+				post-titulo="É isso aí"
+				post-texto="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae fugit necessitatibus optio nostrum eius porro, deleniti odit, perferendis ipsa minima corporis magni commodi, numquam iure animi itaque molestias saepe ex."
+			/>
+		</card-conteudo-vue>
+	</div>
 </template>
 
 <script>
 import CardConteudoVue from '@/components/social/CardConteudoVue'
 import CardDetalheVue from '@/components/social/CardDetalheVue'
+import PublicarConteudoVue from '@/components/social/PublicarConteudoVue'
+// import GridVue from '@/components/layouts/GridVue'
 
 export default {
   	name: 'Home',
@@ -21,7 +26,9 @@ export default {
   	},
   	components:{
 		CardConteudoVue,
-		CardDetalheVue
+		CardDetalheVue,
+		// GridVue,
+		PublicarConteudoVue
 	}
 }
 </script>
