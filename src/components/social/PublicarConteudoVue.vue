9a89fe34fb81ea1c78522895d31e633deb82b2d3
Marcus Camargo <mvocamargo@gmail.com>
2018-06-30 15:45:49 -0300

    Componente de Publicação de Conteúdo

---
+++ src/components/social/PublicarConteudoVue.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="row">
+        <grid-vue class="input-field" tamanho="12">
+            <textarea v-model="postConteudo" id="textConteudo" class="materialize-textarea"></textarea>
+            <label for="textConteudo">Que que pega?</label>
+        </grid-vue>
+        <p v-if="postConteudo">
+            <button class="btn col s2 offset-s10 waves-effect waves-light">Publicar</button>
+        </p>
+    </div>
+</template>
+<script>
+import GridVue from '@/components/layouts/GridVue'
+
+export default {
+    name: 'PublicarConteudoVue',
+    props: [],
+    data () {
+        return {
+            postConteudo: ''
+        }
+    },
+    components: {
+        GridVue
+    }
+}
+</script>
+
