a94540005f0f2d584ea817ccf5f3597ea248168c
Marcus Camargo <mvocamargo@gmail.com>
2018-07-03 22:29:04 -0300

    Separação de Diretórios - Frontend

---
+++ social/src/components/social/CardConteudoVue.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="row">
+        <div class="card">
+            <div class="card-content">
+                <div class="row valign-wrapper">
+                    <grid-vue tamanho="1">
+                        <img :src="perfilFoto" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+                    </grid-vue>
+                    <grid-vue tamanho="11">
+                        <span class="black-text">
+                            <strong>{{perfilNome}}</strong> - <small>{{ postData }}</small>
+                        </span>
+                    </grid-vue>
+                </div>
+                <slot></slot>
+                <div class="card-action">
+                    <!-- <a href="#">This is a link</a> -->
+                    <i class="small material-icons">favorite_border</i>
+                    <i class="small material-icons">insert_comment</i>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import GridVue from '@/components/layouts/GridVue'
+
+export default {
+    name: 'CardConteudoVue',
+    props: ['perfilFoto', 'perfilNome', 'postData'],
+    data () {
+        return {
+
+        }
+    },
+    components: {
+        GridVue
+    }
+}
+</script>
+
---
+++ social/src/components/social/CardDetalheVue.vue
@@ -0,0 +1,25 @@
+<template>
+    <span>
+        <!-- https://materializecss.com/images/sample-1.jpg -->
+        <div v-if="postImagem" class="card-image">
+			<img :src="postImagem">
+			<span class="card-title">{{ postTitulo || '' }}</span>
+		</div>
+		<div v-if="postTexto" class="card-content">
+			<p>{{ postTexto }}</p>
+		</div>
+    </span>
+</template>
+<script>
+
+export default {
+    name: 'CardDetalheVue',
+    props: ['postTitulo', 'postTexto', 'postImagem'],
+    data () {
+        return {
+
+        }
+    }
+}
+</script>
+
