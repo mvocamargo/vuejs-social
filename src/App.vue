eb1c9d7c0c51f415dc6a3c92078ab836abc4e23e
Marcus Camargo <mvocamargo@gmail.com>
2018-07-03 22:24:42 -0300

    Alteração - App - Remoção de itens que viraram componentes

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,18 @@
 <template>
-    <div>
-        <header>
-            <nav-bar cor="pink" url="https://www.google.com" logo="Social"></nav-bar>
-        </header>
-        <main>
-            <div class="container">
-                <div class="row">
-                    <grid-vue tamanho="4">
-                        <card-menu-vue>
-                            <div class="row valign-wrapper">
-                                <grid-vue tamanho="4">
-                                    <img src="https://materializecss.com/images/yuna.jpg" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
-                                </grid-vue>
-                                <grid-vue tamanho="8">
-                                    <span class="black-text">
-                                        <h5>Nome</h5>
-                                        This is a square image. Add the "circle" class to it to make it appear circular.
-                                    </span>
-                                </grid-vue>
-                            </div>
-                        </card-menu-vue>
-                    </grid-vue>
-                    <grid-vue tamanho="8">
-                        <router-view/>
-                    </grid-vue>
-
-                </div>
-            </div>
-        </main>
-        <footer-vue cor="purple" logo="Social" descricao="Descrição do rolê">
-            <li><a class="grey-text text-lighten-3" href="#!">Link</a></li>
-            <li><a class="grey-text text-lighten-3" href="#!">Link</a></li>
-            <li><a class="grey-text text-lighten-3" href="#!">Link</a></li>
-            <li><a class="grey-text text-lighten-3" href="#!">Link</a></li>
-        </footer-vue>
-    </div>
+    <router-view/>
 </template>
 
 <script>
-import NavBar from '@/components/layouts/NavBar'
-import FooterVue from '@/components/layouts/FooterVue'
-import GridVue from '@/components/layouts/GridVue'
-import CardMenuVue from '@/components/layouts/CardMenuVue'
 export default {
 	name: 'App',
 	components:{
-		NavBar,
-		FooterVue,
-        GridVue,
-        CardMenuVue
-	}
+		
+    },
+    data(){
+        return{
+            
+        }
+    }
 }
 </script>
 
