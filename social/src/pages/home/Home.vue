37c5d668ee4bae8444c32b3fb42d90c2286e0275
Marcus Camargo <mvocamargo@gmail.com>
2018-08-18 16:50:42 -0300

    Home - Chamada da imagem do nome do usuário

--- social/src/pages/home/Home.vue
+++ social/src/pages/home/Home.vue
@@ -4,12 +4,11 @@
 			<card-menu-vue>
 				<div class="row valign-wrapper">
 					<grid-vue tamanho="4">
-						<img src="https://materializecss.com/images/yuna.jpg" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+						<img :src="usuario.img" :alt="usuario.name" class="circle responsive-img"> <!-- notice the "circle" class -->
 					</grid-vue>
 					<grid-vue tamanho="8">
 						<span class="black-text">
-							<h5>Nome</h5>
-							This is a square image. Add the "circle" class to it to make it appear circular.
+							<h5>{{usuario.name}}</h5>
 						</span>
 					</grid-vue>
 				</div>
@@ -40,9 +39,16 @@ export default {
   	name: 'Home',
   	data () {
     	return {
-      
+			usuario: false
    		}
-  	},
+	  },
+	  created() {
+        let usuarioAux = sessionStorage.getItem('usuario');
+        if(usuarioAux){
+			this.usuario = JSON.parse(usuarioAux);
+			console.log(usuarioAux);
+        }
+    },
   	components:{
 		CardConteudoVue,
 		CardDetalheVue,
